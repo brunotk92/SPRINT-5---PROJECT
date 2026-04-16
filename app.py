ad5e9d7ee53d6a889174d32836b17cd51e851f6a
Bruno Palmer <bruno.tk92@gmail.com>
2026-04-16 14:12:36 -0300

    Correção final: nome do arquivo requirements e bibliotecas

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+# Estas linhas abaixo NÃO podem ter espaços no começo
+car_data = pd.read_csv('vehicles.csv') 
+hist_button = st.button('Criar histograma')
+
+if hist_button: 
+    # Somente o que está dentro do IF deve ter indentação
+    st.write('Criando um histograma para o conjunto de dados...')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
